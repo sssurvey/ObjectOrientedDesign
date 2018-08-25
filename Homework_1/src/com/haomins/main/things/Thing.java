3a6f1f40f9671879802581323d8aca744a48f045
haominshi <haominshi@gmail.com>
2018-08-24 21:05:25 -0500

    Create Thing.java & Unit Test

---
+++ Homework_1/src/com/haomins/main/things/Thing.java
@@ -0,0 +1,14 @@
+package com.haomins.main.things;
+
+public class Thing {
+
+    private final String name;
+
+    public Thing(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}

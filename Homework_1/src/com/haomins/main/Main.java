f9f05f0e03a24fd4802c1a40b13805e79282f67a
haominshi <haominshi@gmail.com>
2018-08-24 20:43:33 -0500

    Initial Commit

---
+++ Homework_1/src/com/haomins/main/Main.java
@@ -0,0 +1,12 @@
+package com.haomins.main;
+
+public class Main {
+
+    public static String helloWorld() {
+        return "Hello World";
+    }
+
+    public static void main(String[] args) {
+        // write your code here
+    }
+}
---
+++ Homework_1/src/com/haomins/test/MainTest.java
@@ -0,0 +1,17 @@
+package com.haomins.test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import com.haomins.main.Main;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+    @Test
+    void main() {
+        assertEquals(Main.helloWorld(), "Hello World");
+    }
+
+    //TODO add more test ---
+
+}

3b6de301a3b038f30b11f44157d1957b1d262e32
haominshi <haominshi@gmail.com>
2018-09-22 16:57:07 -0500

    Create B.java and added Unittest for B.java

---
+++ Homework_2/src/test/java/BTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BTest {
+
+
+
+    @Test
+    public void getInfoAboutClass() {
+        B testable = new B();
+        assertEquals(new B().getInfoAboutClass(), testable.getInfoAboutClass());
+    }
+
+    @Test
+    public void setEditable() {
+        String testString = "xxx";
+        B testable = new B();
+        testable.setEditable(testString);
+        assertEquals(testString, testable.getEditable());
+    }
+
+    @Test
+    public void getEditable() {
+        String testString = "xxx";
+        B testable = new B();
+        testable.setEditable(testString);
+        assertEquals(testString, testable.getEditable());
+    }
+}

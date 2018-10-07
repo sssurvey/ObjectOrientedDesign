68a292b29b0025a813695e83a8da15554b64648b
haominshi <haominshi@gmail.com>
2018-10-07 16:50:49 -0500

    Finished the main class

--- Homework_3/src/main/java/Main.java
+++ Homework_3/src/main/java/Main.java
@@ -3,10 +3,19 @@ import tableLamp.control.Button;
 
 public class Main {
 
-    Lightbulb lightbulb1 = new Lightbulb();
-    Button buttonToLightbulb1 = new Button(lightbulb1);
+    public static void main(String[] args) {
 
-    public static void main(String [] args){
+        Lightbulb lightbulb1 = new Lightbulb();
+        Button buttonToLightbulb1 = new Button(lightbulb1);
+
+        //turn bulb
+        buttonToLightbulb1.flipTheSwitch(); //switch to ON
+        System.out.println("lightbulb1's current Status ---> " + lightbulb1.getStatus());
+        System.out.println("button1's current Status ---> " + buttonToLightbulb1.getStatus());
+
+        buttonToLightbulb1.flipTheSwitch(); //switch to OFF
+        System.out.println("lightbulb1's current Status ---> " + lightbulb1.getStatus());
+        System.out.println("button1's current Status ---> " + buttonToLightbulb1.getStatus());
     }
 
 }

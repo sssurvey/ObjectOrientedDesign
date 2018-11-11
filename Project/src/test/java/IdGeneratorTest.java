506f48b93649368e20313009989296372c020181
haominshi <haominshi@gmail.com>
2018-11-11 16:34:39 -0600

    added test for util class

--- Project/src/test/java/IdGeneratorTest.java
+++ Project/src/test/java/IdGeneratorTest.java
@@ -2,13 +2,13 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 import util.*;
 
-public class PidGeneratorTest {
+public class IdGeneratorTest {
 
     private static final long MIN_PID = 0l;
     private static final long MAX_PID = 9999999999l;
 
     @Test
-    public void generateTest() {
+    public void generatePidTest() {
         long testLong = IdGenerator.generatePid();
         assertTrue(testLong >= MIN_PID && testLong <= MAX_PID);
     }

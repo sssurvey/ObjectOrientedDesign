b263c86f4fdc9312f5f43ebbcf745b11282185c4
haominshi <haominshi@gmail.com>
2018-11-11 15:07:49 -0600

    created put method, update park, with path var = PID

--- Project/src/main/java/App.java
+++ Project/src/main/java/App.java
@@ -2,10 +2,13 @@ import validator.*;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import util.NumberFormatter;
 
 // the main controller for the program
 @RestController
@@ -20,11 +23,15 @@ public class App {
 
     // Create Park
     @RequestMapping(value = "/parks", method = RequestMethod.POST, consumes = "application/json", produces = "application/json")
-    public String createPark(
-        @RequestBody String park
-    ) throws Exception{
+    public String createPark(@RequestBody String parkJSON) throws Exception {
         ParkValidator validator = new ParkValidator();
-        return validator.parkValidation(park);
+        return validator.parkValidation(parkJSON);
+    }
+
+    // Update Park
+    @RequestMapping(value = "/parks/{PID}", method = RequestMethod.PUT, consumes = "application/json", produces = "application/json")
+    public String updatePark(@PathVariable(value = "PID") int pid) {
+        return NumberFormatter.formatToTenDigitStringPid(pid);
     }
 
     public static void main(String[] args) {
--- Project/src/main/java/util/NumberFormatter.java
+++ Project/src/main/java/util/NumberFormatter.java
@@ -1,13 +1,7 @@
 package util;
 
 public class NumberFormatter {
-
-    private static final String PID_PREFIX = "P";
-
     public static String formatToTenDigitStringPid(long number) {
-        String numberAsString = String.format("%010d", number);
-        StringBuilder stringBuilder = new StringBuilder(PID_PREFIX);
-        stringBuilder.append(numberAsString);
-        return stringBuilder.toString();
+        return String.format("%010d", number);
     }
 }

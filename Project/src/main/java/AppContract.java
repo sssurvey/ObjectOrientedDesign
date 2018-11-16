c5383ad6811f46a0d27e1855c19702e14ba2458b
haominshi <haominshi@gmail.com>
2018-11-15 23:11:16 -0600

    added presenter and contract for main

--- Project/src/main/java/AppContract.java
+++ Project/src/main/java/AppContract.java
@@ -1,3 +1,25 @@
 public interface AppContract {
 
+	public String createPark(String parkJSON);
+
+	public String updatePark(String pid, String parkJSON);
+
+	public String deletePark(String pid);
+
+	public String getAllParks();
+
+	public String getParkDetail(String pid);
+
+	public String createNoteAssociateToPark(String pid, String noteJSON);
+
+	public String getAllNoteAssociateToPark(String pid);
+
+	public String getNoteEntryViaNidAndPid(String pid, String nid);
+
+	public String getAllNotes();
+
+	public String getNoteViaNid(String nid);
+
+	public String updateNoteByPid(String nid, String noteJSON);
+
 }
--- Project/src/main/java/AppPresenter.java
+++ Project/src/main/java/AppPresenter.java
@@ -1,3 +1,58 @@
-public class AppPresenter {
+public class AppPresenter implements AppContract{
+
+	@Override
+	public String createPark(String parkJSON) {
+		return null;
+	}
+
+	@Override
+	public String updatePark(String pid, String parkJSON) {
+		return null;
+	}
+
+	@Override
+	public String deletePark(String pid) {
+		return null;
+	}
+
+	@Override
+	public String getAllParks() {
+		return null;
+	}
+
+	@Override
+	public String getParkDetail(String pid) {
+		return null;
+	}
+
+	@Override
+	public String createNoteAssociateToPark(String pid, String noteJSON) {
+		return null;
+	}
+
+	@Override
+	public String getAllNoteAssociateToPark(String pid) {
+		return null;
+	}
+
+	@Override
+	public String getNoteEntryViaNidAndPid(String pid, String nid) {
+		return null;
+	}
+
+	@Override
+	public String getAllNotes() {
+		return null;
+	}
+
+	@Override
+	public String getNoteViaNid(String nid) {
+		return null;
+	}
+
+	@Override
+	public String updateNoteByPid(String nid, String noteJSON) {
+		return null;
+	}
     
 }
